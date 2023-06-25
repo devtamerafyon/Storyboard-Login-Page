ddab098c0565fd367630b3f7710792ed1895f0b3
Tamer Afyon <101478641+devtamerafyon@users.noreply.github.com>
2023-06-26 00:29:12 +0300

    Initial Commit

---
+++ ViewControllerProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerProjesi
+//
+//  Created by Tamer Afyon on 26.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
